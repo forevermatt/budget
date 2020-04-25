3ae5a71a7bd457130f6c98233e6f9836c4357712
forevermatt <forevermatt@users.noreply.github.com>
2020-04-25 14:51:58 -0400

    Add methods for updating a category

--- src/data/categories.js
+++ src/data/categories.js
@@ -15,3 +15,23 @@ export const create = name => {
 const addItemToStore = (newItem, store) => {
   store.update(existingItems => [newItem, ...existingItems])
 }
+
+export const update = (uuid, changes) => {
+  updateItemInStore(uuid, changes, categories)
+}
+
+const updateItemInStore = (uuid, changes, store) => {
+  store.update(existingItems => {
+    const itemToUpdate = existingItems.find(item => item.uuid === uuid)
+    const updatedItem = Object.assign({}, itemToUpdate, changes)
+    
+    for (let i = 0; i < existingItems.length; i++) {
+      if (existingItems[i].uuid === uuid) {
+        existingItems[i] = updatedItem
+        break
+      }
+    }
+    
+    return existingItems
+  })
+}
