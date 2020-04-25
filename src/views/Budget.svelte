ba9b4dbae9691f5f852a2b8f1113b594f889a625
forevermatt <forevermatt@users.noreply.github.com>
2020-04-25 14:52:37 -0400

    Show the current categories

--- src/views/Budget.svelte
+++ src/views/Budget.svelte
@@ -1,1 +1,13 @@
+<script>
+import { categories } from '../data/categories'
+</script>
+
 <h1>Budget</h1>
+
+<ul>
+  {#each $categories as category}
+    <li>{category.name}: ${category.amount}</li>
+  {/each}
+</ul>
+
+<a href="#/category/new">Add new category</a>
