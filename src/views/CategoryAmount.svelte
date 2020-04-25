c1e967b46791e885e78857d62dfb4e26a46531af
forevermatt <forevermatt@users.noreply.github.com>
2020-04-25 14:52:26 -0400

    Enable specifying an amount for a new category

--- src/views/CategoryAmount.svelte
+++ src/views/CategoryAmount.svelte
@@ -1,11 +1,22 @@
 <script>
-import { categories } from '../data/categories'
+import { categories, update } from '../data/categories'
+import { push } from 'svelte-spa-router'
 
 export let params // URL parameters provider by router.
 
+let amount = 0
+
 $: uuid = params.uuid
 $: category = $categories.find(category => category.uuid === uuid) || {}
+
+function onSubmit() {
+  update(uuid, {amount})
+  push(`/budget`)
+}
 </script>
 
 <h1>Amount for {category.name}</h1>
 
+<form on:submit={onSubmit}>
+  <input type="number" step="0.01" min="0" bind:value={amount} />
+</form>
