fda89a6b671015ed7c44d146388ecdb698663469
forevermatt <forevermatt@users.noreply.github.com>
2020-06-20 12:23:14 -0400

    Add a way to specify "who" (for a transaction)

---
+++ src/components/WhoSelector.svelte
@@ -0,0 +1,17 @@
+<script>
+import { createEventDispatcher } from 'svelte';
+
+export let title
+export let who = ''
+
+const dispatch = createEventDispatcher();
+
+const recordWho = () => {
+  dispatch('select', who)
+}
+</script>
+
+<form class="pad-vertical text-center" on:submit|preventDefault={recordWho}>
+  <label for="who"><b>{ title }</b></label>
+  <input type="text" id="who" bind:value={who}>
+</form>
