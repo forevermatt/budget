c1bffad6daf27423027babf321372f91aefe6023
forevermatt <forevermatt@users.noreply.github.com>
2020-07-18 11:43:58 -0400

    Add components for showing a transactions categories/amounts

---
+++ src/components/CategoryTag.svelte
@@ -0,0 +1,8 @@
+<script>
+import { formatAmount } from '../helpers/numbers'
+
+export let name = ''
+export let amount = 0
+</script>
+
+<span class="badge badge-info">{name}: $ {formatAmount(amount)}</span>
---
+++ src/components/CategoryTags.svelte
@@ -0,0 +1,14 @@
+<script>
+import CategoryTag from '../components/CategoryTag.svelte'
+import { categories, getCategoryFrom } from '../data/categories'
+
+export let transaction = {}
+
+$: categoryAmounts = transaction.categoryAmounts || {}
+$: categoryUuids = Object.keys(categoryAmounts)
+</script>
+
+{#each categoryUuids as categoryUuid}
+  <CategoryTag name={getCategoryFrom(categoryUuid, $categories).name}
+               amount={categoryAmounts[categoryUuid]} />
+{/each}
