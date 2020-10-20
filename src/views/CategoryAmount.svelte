007b7b92fe61695959db0953f053523bc5673560
forevermatt <forevermatt@users.noreply.github.com>
2020-10-19 20:48:33 -0400

    Add buttons to the bottom of category/uuid/amount page

--- src/views/CategoryAmount.svelte
+++ src/views/CategoryAmount.svelte
@@ -1,7 +1,10 @@
 <script>
 import AmountInput from '../components/AmountInput.svelte'
+import Button from '../components/Button.svelte'
+import ButtonRow from '../components/ButtonRow.svelte'
 import { getBudgetedFor, setBudgetedForCategory } from '../data/budget'
 import { categories } from '../data/categories'
+import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { push } from 'svelte-spa-router'
 
 export let params // URL parameters provider by router.
@@ -10,13 +13,19 @@ $: uuid = params.uuid
 $: category = $categories.find(category => category.uuid === uuid) || {}
 $: initialAmount = category && getBudgetedFor(uuid) || 0
 
-function onSubmit(event) {
-  let amount = event.detail
-  setBudgetedForCategory(uuid, amount)
+let resultingAmount = 0
+
+const onAmount = () => {
+  setBudgetedForCategory(uuid, resultingAmount)
   push(`/budget`)
 }
 </script>
 
 <h2>Monthly amount for {category.name}</h2>
 
-<AmountInput amount={initialAmount} on:next={onSubmit} />
+<AmountInput amount={initialAmount} on:next={onAmount} bind:resultingAmount={resultingAmount} />
+
+<ButtonRow>
+  <Button icon={faCheck} name="save" on:click={onAmount} />
+  <Button icon={faTimes} name="cancel" url="#/budget" left />
+</ButtonRow>
