ee1f95391361c290be403f024b73349a381948d8
forevermatt <forevermatt@users.noreply.github.com>
2025-08-08 15:14:31 -0400

    Begin trying to build a migration path from localStorage to pouchDB

--- src/data/accounts.js
+++ src/data/accounts.js
@@ -1,3 +1,4 @@
+import { database } from './database'
 import { addToList, updateInList } from '../helpers/data-store-helpers'
 import { getListFromStorage, saveToStorage } from './storage'
 import { get, writable } from 'svelte/store'
@@ -5,7 +6,7 @@ import { v4 as uuidv4 } from 'uuid'
 
 const ACCOUNTS = 'accounts'
 
-const accounts = writable([])
+export const accounts = writable([])
 
 export const createAccount = name => {
   const newAccount = {
@@ -17,8 +18,8 @@ export const createAccount = name => {
   return newAccount
 }
 
-export const getAccount = (uuid) => {
-  const accounts = listAccounts()
+export const getAccount = async (uuid) => {
+  const accounts = await listAccounts()
   return getAccountFrom(uuid, accounts)
 }
 
@@ -26,12 +27,79 @@ const getAccountFrom = (uuid, list) => {
   return list.find(item => item.uuid === uuid) || {}
 }
 
-export const listAccounts = () => {
+export const listAccounts = async () => {
   return getListFromStorage(ACCOUNTS)
 }
 
-export const loadAccounts = () => {
-  accounts.set(getListFromStorage(ACCOUNTS))
+export const loadAccounts = async () => {
+  await migrateFromStorage()
+  try {
+    const response = await database.allDocs({include_docs: true, descending: true})
+    const rows = response.rows
+    accounts.set(rows.map(row => row.doc))
+  } catch (reason) {
+    console.error(
+      'Error while loading accounts:',
+      reason
+    )
+  }
+}
+
+const migrateFromStorage = async () => {
+  const accountsFromStorage = getListFromStorage(ACCOUNTS)
+  if (accountsFromStorage.length > 0) {
+    const accountsForPouchDB = accountsFromStorage.map(convertAccountUuidToId)
+    try {
+      const results = await database.bulkDocs(accountsForPouchDB)
+      console.debug(
+        'Converted accounts from localStorage to PouchDB.',
+        'localStorage:',
+        accountsFromStorage,
+        'PouchDB:',
+        accountsForPouchDB,
+        'results:',
+        results
+      )
+      await removeMovedAccountsFromStorage(results, accountsFromStorage)
+      // results.forEach(result => {
+      //   if (result.ok) {
+      //     delete accountsFromStorage[result.id]
+      //   }
+      // })
+      // saveToStorage(ACCOUNTS, accountsFromStorage)
+    } catch (reason) {
+      console.error(
+        'Error while accounts from localStorage to PouchDB:',
+        reason
+      )
+    }
+  }
+}
+
+const convertAccountUuidToId = (account) => {
+  const newAccount = {
+    ...account,
+  }
+  newAccount._id = account.uuid
+  delete newAccount.uuid
+  return newAccount
+}
+
+const removeMovedAccountsFromStorage = (results, accountsFromStorage) => {
+  results.forEach(result => {
+    if (result.ok) {
+      removeAccountFromList(result.id, accountsFromStorage)
+    }
+  })
+  saveToStorage(ACCOUNTS, accountsFromStorage)
+}
+
+const removeAccountFromList = (accountUuid, accountsList) => {
+  for (let i = 0; i < accountsList.length; i++) {
+    if (accountUuid === accountsList[i].uuid) {
+      delete accountsList[i]
+    }
+  }
 }
 
 const saveAccounts = () => saveToStorage(ACCOUNTS, get(accounts))
