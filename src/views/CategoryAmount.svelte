65ac071d5cb950daeaa9ec0988eb86f8ccf5e5a7
forevermatt <forevermatt@users.noreply.github.com>
2025-08-21 16:02:01 -0400

    Avoid showing 'undefined' for name as category is loading

--- src/views/CategoryAmount.svelte
+++ src/views/CategoryAmount.svelte
@@ -26,7 +26,7 @@ onMount(async () => {
 })
 </script>
 
-<h2>Monthly amount for {category.name}</h2>
+<h2>Monthly amount for {category.name || '...'}</h2>
 
 <AmountInput amount={initialAmount} on:next={onAmount} bind:resultingAmount={resultingAmount} />
 
