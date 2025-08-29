8b23baab9aebad99d506dadf1344f87043d01069
forevermatt <forevermatt@users.noreply.github.com>
2025-08-29 15:29:50 -0400

    Remove unneeded (mostly category-related) code

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,14 @@
 <script>
 import ErrorMessage from './components/ErrorMessage.svelte'
-import { loadAccounts } from './data/accounts'
 import { loadBudget, refillBudgetCategories } from './data/budget'
-import { loadCategories } from './data/categories'
 import { loadTransactions } from './data/transactions'
 import { onMount } from 'svelte'
 import Router from 'svelte-spa-router'
 import routes from './views/routes'
 
 onMount(async () => {
-  loadAccounts()
-  loadCategories()
+  // loadAccounts()
+  // loadCategories()
   loadBudget()
   loadTransactions()
   refillBudgetCategories()
--- src/data/categories.js
+++ src/data/categories.js
@@ -1,13 +1,6 @@
 import database from './database'
-import { addToList, updateInList } from '../helpers/data-store-helpers'
-import { getListFromStorage, saveToStorage } from './storage'
-import { get, writable } from 'svelte/store'
-import { v4 as uuidv4 } from 'uuid'
 
-const CATEGORIES = 'categories'
 const ITEM_TYPE_PREFIX = 'c'
-
-const categories = writable([])
 
 export const addCategory = async name => database.insert(ITEM_TYPE_PREFIX, { name })
 
@@ -15,17 +8,7 @@ export const deleteCategory = (id) => database.deleteItem(id)
 
 export const getCategory = async id => database.get(id)
 
-const getCategoryFrom = (id, list) => {
-  return list.find(item => item.id === id) || {}
-}
-
 export const listCategories = async () => database.list(ITEM_TYPE_PREFIX)
-
-export const loadCategories = () => {
-  categories.set(getListFromStorage(CATEGORIES))
-}
-
-const saveCategories = () => saveToStorage(CATEGORIES, get(categories))
 
 export const updateCategory = async (id, changes) => {
   const existing = await getCategory(id)
