933ffe5f74cb77c14d64808ed2da9c57679cc27b
forevermatt <forevermatt@users.noreply.github.com>
2025-09-04 16:48:44 -0400

    Fix account- and category-selectors to use `_id` (not `id`)
    PouchDB records returned seem to have an underscore before metadata fields, like id and rev.

--- src/components/AccountSelector.svelte
+++ src/components/AccountSelector.svelte
@@ -6,9 +6,9 @@ export let accounts = []
 const dispatch = createEventDispatcher();
 </script>
 
-{#each accounts as account (account.id)}
+{#each accounts as account (account._id)}
   <p>
-    <button class="btn btn-outline-secondary" on:click={() => dispatch('select', account.id)}>
+    <button class="btn btn-outline-secondary" on:click={() => dispatch('select', account._id)}>
       {account.name}
     </button>
   </p>
