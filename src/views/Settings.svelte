83530e1b62cfa6bc4391da43c90011f6d290d3ae
forevermatt <forevermatt@users.noreply.github.com>
2025-09-26 15:04:51 -0400

    Begin adding Settings view for configuring remote sync

---
+++ src/views/Settings.svelte
@@ -0,0 +1,37 @@
+<script>
+import Button from '../components/Button.svelte'
+import ButtonRow from '../components/ButtonRow.svelte'
+import Form from '../components/Form.svelte'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
+
+let server = ''
+let username = ''
+let password = ''
+
+const onSyncFormSubmit = async () => {
+  console.log(server, username, password) // TEMP
+}
+</script>
+
+<h2>Settings</h2>
+
+<h3>Sync</h3>
+<Form on:submit={onSyncFormSubmit}>
+  <label>
+    Server:
+    <input class="form-control" name="server" placeholder="Server" bind:value={server} />
+  </label>
+  <label>
+    Username:
+    <input class="form-control" name="username" placeholder="Username" bind:value={username} />
+  </label>
+  <label>
+    Password:
+    <input class="form-control" name="password" placeholder="Password" type="password" bind:value={password} />
+  </label>
+  <button type="submit">Save</button>
+</Form>
+
+<ButtonRow>
+  <Button icon={faHome} name="budget" url="#/budget" left />
+</ButtonRow>
